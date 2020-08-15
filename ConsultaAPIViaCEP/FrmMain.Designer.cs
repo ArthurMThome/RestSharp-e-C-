bd753cccb453bcb8b620fa7dd899f08b66add547
Arthur Martins Thomé <40119529+ArthurMThome@users.noreply.github.com>
2020-08-15 19:35:11 -0300

    Criando projeto e finalizando!!

---
+++ ConsultaAPIViaCEP/FrmMain.Designer.cs
@@ -0,0 +1,359 @@
+﻿namespace ConsultaAPIViaCEP
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnSair = new System.Windows.Forms.Button();
+            this.btnConsultar = new System.Windows.Forms.Button();
+            this.txtCEP = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtLogradouro = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtComplemento = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtBairro = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtLocalidade = new System.Windows.Forms.TextBox();
+            this.txtUF = new System.Windows.Forms.TextBox();
+            this.txtIBGE = new System.Windows.Forms.TextBox();
+            this.txtUnidade = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.txtGIA = new System.Windows.Forms.TextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.txtCEPBuscar = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(94, 74);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(530, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Digite o CEP que deseja consultar (apenas números):";
+            // 
+            // btnSair
+            // 
+            this.btnSair.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnSair.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSair.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnSair.Location = new System.Drawing.Point(535, 560);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(119, 36);
+            this.btnSair.TabIndex = 2;
+            this.btnSair.Text = "SAIR";
+            this.btnSair.UseVisualStyleBackColor = false;
+            this.btnSair.Click += new System.EventHandler(this.btnSair_Click);
+            // 
+            // btnConsultar
+            // 
+            this.btnConsultar.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnConsultar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConsultar.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnConsultar.Location = new System.Drawing.Point(35, 159);
+            this.btnConsultar.Name = "btnConsultar";
+            this.btnConsultar.Size = new System.Drawing.Size(619, 36);
+            this.btnConsultar.TabIndex = 2;
+            this.btnConsultar.Text = "CONSULTAR";
+            this.btnConsultar.UseVisualStyleBackColor = false;
+            this.btnConsultar.Click += new System.EventHandler(this.btnConsultar_Click);
+            // 
+            // txtCEP
+            // 
+            this.txtCEP.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCEP.Location = new System.Drawing.Point(183, 220);
+            this.txtCEP.Name = "txtCEP";
+            this.txtCEP.ReadOnly = true;
+            this.txtCEP.Size = new System.Drawing.Size(86, 23);
+            this.txtCEP.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(29, 25);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(628, 31);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Consultar CEP através de uma API do ViaCEP.";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(138, 223);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 17);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "CEP:";
+            // 
+            // txtLogradouro
+            // 
+            this.txtLogradouro.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtLogradouro.Location = new System.Drawing.Point(183, 254);
+            this.txtLogradouro.Name = "txtLogradouro";
+            this.txtLogradouro.ReadOnly = true;
+            this.txtLogradouro.Size = new System.Drawing.Size(435, 23);
+            this.txtLogradouro.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(91, 257);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(86, 17);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Logradouro:";
+            // 
+            // txtComplemento
+            // 
+            this.txtComplemento.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtComplemento.Location = new System.Drawing.Point(183, 292);
+            this.txtComplemento.Name = "txtComplemento";
+            this.txtComplemento.ReadOnly = true;
+            this.txtComplemento.Size = new System.Drawing.Size(435, 23);
+            this.txtComplemento.TabIndex = 4;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(79, 295);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(98, 17);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Complemento:";
+            // 
+            // txtBairro
+            // 
+            this.txtBairro.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBairro.Location = new System.Drawing.Point(183, 332);
+            this.txtBairro.Name = "txtBairro";
+            this.txtBairro.ReadOnly = true;
+            this.txtBairro.Size = new System.Drawing.Size(86, 23);
+            this.txtBairro.TabIndex = 4;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(127, 332);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(50, 17);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Bairro:";
+            // 
+            // txtLocalidade
+            // 
+            this.txtLocalidade.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtLocalidade.Location = new System.Drawing.Point(183, 369);
+            this.txtLocalidade.Name = "txtLocalidade";
+            this.txtLocalidade.ReadOnly = true;
+            this.txtLocalidade.Size = new System.Drawing.Size(86, 23);
+            this.txtLocalidade.TabIndex = 4;
+            // 
+            // txtUF
+            // 
+            this.txtUF.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUF.Location = new System.Drawing.Point(183, 403);
+            this.txtUF.Name = "txtUF";
+            this.txtUF.ReadOnly = true;
+            this.txtUF.Size = new System.Drawing.Size(86, 23);
+            this.txtUF.TabIndex = 4;
+            // 
+            // txtIBGE
+            // 
+            this.txtIBGE.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtIBGE.Location = new System.Drawing.Point(183, 481);
+            this.txtIBGE.Name = "txtIBGE";
+            this.txtIBGE.ReadOnly = true;
+            this.txtIBGE.Size = new System.Drawing.Size(86, 23);
+            this.txtIBGE.TabIndex = 4;
+            // 
+            // txtUnidade
+            // 
+            this.txtUnidade.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUnidade.Location = new System.Drawing.Point(183, 441);
+            this.txtUnidade.Name = "txtUnidade";
+            this.txtUnidade.ReadOnly = true;
+            this.txtUnidade.Size = new System.Drawing.Size(86, 23);
+            this.txtUnidade.TabIndex = 4;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(96, 372);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(81, 17);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Localidade:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(133, 484);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(44, 17);
+            this.label8.TabIndex = 6;
+            this.label8.Text = "IBGE:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(147, 406);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(30, 17);
+            this.label9.TabIndex = 6;
+            this.label9.Text = "UF:";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(112, 441);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(65, 17);
+            this.label10.TabIndex = 6;
+            this.label10.Text = "Unidade:";
+            // 
+            // txtGIA
+            // 
+            this.txtGIA.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtGIA.Location = new System.Drawing.Point(183, 518);
+            this.txtGIA.Name = "txtGIA";
+            this.txtGIA.ReadOnly = true;
+            this.txtGIA.Size = new System.Drawing.Size(86, 23);
+            this.txtGIA.TabIndex = 4;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.Location = new System.Drawing.Point(142, 521);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(35, 17);
+            this.label11.TabIndex = 6;
+            this.label11.Text = "GIA:";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label12.Location = new System.Drawing.Point(222, 635);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(266, 17);
+            this.label12.TabIndex = 7;
+            this.label12.Text = "Projeto criado por Arthur Martins Thomé.";
+            // 
+            // txtCEPBuscar
+            // 
+            this.txtCEPBuscar.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCEPBuscar.Location = new System.Drawing.Point(283, 113);
+            this.txtCEPBuscar.MaxLength = 8;
+            this.txtCEPBuscar.Name = "txtCEPBuscar";
+            this.txtCEPBuscar.Size = new System.Drawing.Size(114, 30);
+            this.txtCEPBuscar.TabIndex = 8;
+            this.txtCEPBuscar.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(684, 661);
+            this.Controls.Add(this.txtCEPBuscar);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtUnidade);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtGIA);
+            this.Controls.Add(this.txtComplemento);
+            this.Controls.Add(this.txtIBGE);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtUF);
+            this.Controls.Add(this.txtBairro);
+            this.Controls.Add(this.txtLocalidade);
+            this.Controls.Add(this.txtLogradouro);
+            this.Controls.Add(this.txtCEP);
+            this.Controls.Add(this.btnConsultar);
+            this.Controls.Add(this.btnSair);
+            this.Controls.Add(this.label1);
+            this.Name = "FrmMain";
+            this.Text = "Consultando CEP";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnSair;
+        private System.Windows.Forms.Button btnConsultar;
+        private System.Windows.Forms.TextBox txtCEP;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtLogradouro;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtComplemento;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtBairro;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtLocalidade;
+        private System.Windows.Forms.TextBox txtUF;
+        private System.Windows.Forms.TextBox txtIBGE;
+        private System.Windows.Forms.TextBox txtUnidade;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox txtGIA;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.TextBox txtCEPBuscar;
+    }
+}
+
---
+++ ConsultaAPIViaCEP/FrmMain.cs
@@ -0,0 +1,156 @@
+﻿/// Arthur M. Thomé
+/// 15 AUG 2020
+/// Arthur Martins Thomé - Projeto RestSharp - Via CEP.
+
+#region Statements
+
+using RestSharp;
+using RestSharp.Deserializers;
+using RestSharp.Serialization.Json;
+
+using System;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.Windows.Forms;
+using System.ComponentModel;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+#endregion
+
+namespace ConsultaAPIViaCEP
+{
+    public partial class FrmMain : Form
+    {
+        #region Form Methods
+
+        public FrmMain ( )
+        {
+            InitializeComponent ( );
+        }
+
+        #endregion
+
+        #region Buttons Click
+
+        /// <summary>
+        /// Função chamada ao clicar no botão de consultar.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnConsultar_Click ( object sender, EventArgs e )
+        {
+            // Primeira coisa a fazer quando clicar no botão é limpar os campos.
+            txtUF.Text = "";
+            txtGIA.Text = "";
+            txtCEP.Text = "";
+            txtIBGE.Text = "";
+            txtBairro.Text = "";
+            txtUnidade.Text = "";
+            txtLogradouro.Text = "";
+            txtLocalidade.Text = "";
+            txtComplemento.Text = "";
+
+            try
+            {
+                // Checar se o usuário digitou os 9 dígitos no campo do CEP.
+                if ( txtCEPBuscar.TextLength == 8 )
+                {
+                    //Formatar a string do BaseURL.
+                    string _baseURL = string.Format ( "https://viacep.com.br/ws/{0}/json/", txtCEPBuscar.Text );
+                    // Abrir Client do RestSharp.
+                    RestClient _restClient = new RestClient ( _baseURL );
+                    // Inicializar a requisição completa.
+                    RestRequest _restRequest = new RestRequest ( Method.GET );
+                    // Trazer os objetos de retorno, vai informar se foi bem sucedida ou não.
+                    IRestResponse _restResponse = _restClient.Execute ( _restRequest );
+
+                    // Tratando possíveis erros.
+                    // ContentLength -1 quer dizer "BadRequest", alguma coisa aconteceu de errado na requisição.
+                    if ( _restResponse.StatusCode == System.Net.HttpStatusCode.BadRequest )
+                    {
+                        // Mostrar ums mensagem para informar o usuário.
+                        MessageBox.Show ( "Houve um problema com sua requisição: " + _restResponse.Content, "Erro!", MessageBoxButtons.OK, MessageBoxIcon.Exclamation );
+                    }
+                    else
+                    {
+                        // Pegar os dados do JSON e transformar em um objeto da classe DadosRetorno.
+                        DadosRetorno _dadosRetorno = new JsonDeserializer ( ).Deserialize < DadosRetorno > ( _restResponse );
+
+                        // Checar se o CEP de retorno é null.
+                        if ( string.IsNullOrEmpty ( _dadosRetorno.cep ) )
+                        {
+                            /*
+                             * Por algum motivo que eu desconheço, essa formatação nao está funcionando.
+                             * Quando eu descobrir o motivo, vou arrumar o código!
+                            */
+                            // Formatar cep para mostrar no erro.
+                            string _cep = string.Format ( "{0:00000-000}", txtCEPBuscar.Text );
+
+                            MessageBox.Show ( "CEP: " + _cep + ", não encontrado. ", "Atenção!", MessageBoxButtons.OK, MessageBoxIcon.Exclamation );
+                            return;
+                        }
+
+                        // Preencher os campos do Windons Forms.
+                        txtUF.Text = _dadosRetorno.uf;
+                        txtGIA.Text = _dadosRetorno.gia;
+                        txtCEP.Text = _dadosRetorno.cep;
+                        txtIBGE.Text = _dadosRetorno.ibge;
+                        txtBairro.Text = _dadosRetorno.bairro;
+                        txtUnidade.Text = _dadosRetorno.unidade;
+                        txtLogradouro.Text = _dadosRetorno.logradouro;
+                        txtLocalidade.Text = _dadosRetorno.localidade;
+                        txtComplemento.Text = _dadosRetorno.complemento;
+
+                        // Limpar texto do label de CEP para pesquisar.
+                        txtCEPBuscar.Text = "";
+                    }
+                }
+                else
+                {
+                    // Erro pois o usuário nao digitou os 9 digitos do CEP.
+                    MessageBox.Show ( "O CEP não está completo, você esqueceu algum dígito!", "Atenção!", MessageBoxButtons.OK, MessageBoxIcon.Exclamation );
+                }
+            }
+            catch ( Exception erro )
+            {
+                // Algum erro sem tratamento, mostrar um erro geral.
+                MessageBox.Show ( "Erro geral ao consultar a API: " + erro.Message, "Erro!", MessageBoxButtons.OK, MessageBoxIcon.Error );
+            }
+        }
+
+        /// <summary>
+        /// Função chamada ao clicar no botão sair.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnSair_Click ( object sender, EventArgs e )
+        {
+            // Mostrar Message Box perguntando se o usuário quer mesmo sair da aplicação.
+            if ( MessageBox.Show ( "Obrigado por utilizar minha aplicação. " +
+                                   "\nEspero que tenha gostado! " +
+                                   "\n\nSair da aplicação?", "Saindo...", MessageBoxButtons.YesNo, MessageBoxIcon.Question ) == DialogResult.Yes )
+            {
+                // Caso ele clique no botão "YES", fechar a aplicação;
+                Application.Exit ( );
+            }
+        }
+
+        #endregion
+    }
+
+    public class DadosRetorno
+    {
+        public string uf { get; set; }
+        public string cep { get; set; }
+        public string gia { get; set; }
+        public string ibge { get; set; }
+        public string bairro { get; set; }
+        public string unidade { get; set; }
+        public string logradouro { get; set; }
+        public string localidade { get; set; }
+        public string complemento { get; set; }
+    }
+}
